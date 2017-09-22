b264b28a9b7e5cf57aad2ef82fc83b6c1797f2ac
tinghuiW <tinghui@ualberta.ca>
2017-09-22 13:03:21 -0600

    fork the original files(lab2) from guanfang. I write clear function

---
+++ LonelyTwitterActivity.java
@@ -0,0 +1,131 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LonelyTwitterActivity extends Activity {
+
+	private static final String FILENAME = "file.sav";
+	private EditText bodyText;
+	private ListView oldTweetsList;
+
+	private ArrayList<Tweet> tweetList;
+	private ArrayAdapter<Tweet> adapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bodyText = (EditText) findViewById(R.id.body);
+		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+
+
+				tweetList.add(new NormalTweet(text));
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+
+			}
+		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+
+
+				tweetList.clear();
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		loadFromFile();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
+		oldTweetsList.setAdapter(adapter);
+	}
+
+	private void loadFromFile() {
+		try {
+			FileInputStream fis = openFileInput(FILENAME);
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+			Gson gson = new Gson();
+
+			//Taken from https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+			// 2017-09-19
+			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+			tweetList = gson.fromJson(in, listType);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			tweetList = new ArrayList<Tweet>();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		}
+
+	}
+	
+	private void saveInFile() {
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME,
+					Context.MODE_PRIVATE);
+
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+			Gson gson = new Gson();
+			gson.toJson(tweetList, out);
+			out.flush();
+
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		}
+	}
+}
